d386ced6af0f3333b0fae29e319eaf54f5a5b3ae
kevinvera-wr <160569137+kevinvera-wr@users.noreply.github.com>
2024-02-20 00:13:27 -0500

    First upload
    First upload for the AI

---
+++ V-V/loverbot.js
@@ -0,0 +1,388 @@
+
+function loverbot(noRandomFlag) {
+	this.noRandom= (noRandomFlag)? true:false;
+	this.capitalizeFirstLetter=false;
+	this.debug=false;
+	this.memSize=100;//originally 20
+	this.version="v1.2 ( lover.os original)";
+	if (!this._dataParsed) this._init();
+	this.reset();
+}
+
+loverbot.prototype.reset = function() {
+	this.quit=false;
+	this.mem=[];
+	this.lastchoice=[];
+	for (var k=0; k<loverKeywords.length; k++) {
+		this.lastchoice[k]=[];
+		var rules=loverKeywords[k][2];
+		for (var i=0; i<rules.length; i++) this.lastchoice[k][i]=-1;
+	}
+}
+
+loverbot.prototype._dataParsed = false;
+
+loverbot.prototype._init = function() {
+	// install ref to global object
+	var global=loverbot.prototype.global=self;
+	// parse data and convert it from canonical form to internal use
+	// produce synonym list
+	var synPatterns={};
+	if ((global.loverSynons) && (typeof loverSynons == 'object')) {
+		for (var i in loverSynons) synPatterns[i]='('+i+'|'+loverSynons[i].join('|')+')';
+	}
+	// check for keywords or install empty structure to prevent any errors
+	if ((!global.loverKeywords) || (typeof loverKeywords.length == 'undefined')) {
+		loverKeywords=[['###',0,[['###',[]]]]];
+	}
+	// 1st convert rules to regexps
+	// expand synonyms and insert asterisk expressions for backtracking
+	var sre=/@(\S+)/;
+	var are=/(\S)\s*\*\s*(\S)/;
+	var are1=/^\s*\*\s*(\S)/;
+	var are2=/(\S)\s*\*\s*$/;
+	var are3=/^\s*\*\s*$/;
+	var wsre=/\s+/g;
+	for (var k=0; k<loverKeywords.length; k++) {
+		var rules=loverKeywords[k][2];
+		loverKeywords[k][3]=k; // save original index for sorting
+		for (var i=0; i<rules.length; i++) {
+			var r=rules[i];
+			// check mem flag and store it as decomp's element 2
+			if (r[0].charAt(0)=='$') {
+				var ofs=1;
+				while (r[0].charAt[ofs]==' ') ofs++;
+				r[0]=r[0].substring(ofs);
+				r[2]=true;
+			}
+			else {
+				r[2]=false;
+			}
+			// expand synonyms (v.1.1: work around lambda function)
+			var m=sre.exec(r[0]);
+			while (m) {
+				var sp=(synPatterns[m[1]])? synPatterns[m[1]]:m[1];
+				r[0]=r[0].substring(0,m.index)+sp+r[0].substring(m.index+m[0].length);
+				m=sre.exec(r[0]);
+			}
+			// expand asterisk expressions (v.1.1: work around lambda function)
+			if (are3.test(r[0])) {
+				r[0]='\\s*(.*)\\s*';
+			}
+			else {
+				m=are.exec(r[0]);
+				if (m) {
+					var lp='';
+					var rp=r[0];
+					while (m) {
+						lp+=rp.substring(0,m.index+1);
+						if (m[1]!=')') lp+='\\b';
+						lp+='\\s*(.*)\\s*';
+						if ((m[2]!='(') && (m[2]!='\\')) lp+='\\b';
+						lp+=m[2];
+						rp=rp.substring(m.index+m[0].length);
+						m=are.exec(rp);
+					}
+					r[0]=lp+rp;
+				}
+				m=are1.exec(r[0]);
+				if (m) {
+					var lp='\\s*(.*)\\s*';
+					if ((m[1]!=')') && (m[1]!='\\')) lp+='\\b';
+					r[0]=lp+r[0].substring(m.index-1+m[0].length);
+				}
+				m=are2.exec(r[0]);
+				if (m) {
+					var lp=r[0].substring(0,m.index+1);
+					if (m[1]!='(') lp+='\\b';
+					r[0]=lp+'\\s*(.*)\\s*';
+				}
+			}
+			// expand white space
+			r[0]=r[0].replace(wsre, '\\s+');
+			wsre.lastIndex=0;
+		}
+	}
+	// now sort keywords by rank (highest first)
+	loverKeywords.sort(this._sortKeywords);
+	// and compose regexps and refs for pres and posts
+	loverbot.prototype.pres={};
+	loverbot.prototype.posts={};
+	if ((global.loverPres) && (loverPres.length)) {
+		var a=new Array();
+		for (var i=0; i<loverPres.length; i+=2) {
+			a.push(loverPres[i]);
+			loverbot.prototype.pres[loverPres[i]]=loverPres[i+1];
+		}
+		loverbot.prototype.preExp = new RegExp('\\b('+a.join('|')+')\\b');
+	}
+	else {
+		// default (should not match)
+		loverbot.prototype.preExp = /####/;
+		loverbot.prototype.pres['####']='####';
+	}
+	if ((global.loverPosts) && (loverPosts.length)) {
+		var a=new Array();
+		for (var i=0; i<loverPosts.length; i+=2) {
+			a.push(loverPosts[i]);
+			loverbot.prototype.posts[loverPosts[i]]=loverPosts[i+1];
+		}
+		loverbot.prototype.postExp = new RegExp('\\b('+a.join('|')+')\\b');
+	}
+	else {
+		// default (should not match)
+		loverbot.prototype.postExp = /####/;
+		loverbot.prototype.posts['####']='####';
+	}
+	// check for loverQuits and install default if missing
+	if ((!global.loverQuits) || (typeof loverQuits.length == 'undefined')) {
+		loverQuits=[];
+	}
+	// done
+	loverbot.prototype._dataParsed=true;
+}
+
+loverbot.prototype._sortKeywords = function(a,b) {
+	// sort by rank
+	if (a[1]>b[1]) return -1
+	else if (a[1]<b[1]) return 1
+	// or original index
+	else if (a[3]>b[3]) return 1
+	else if (a[3]<b[3]) return -1
+	else return 0;
+}
+
+loverbot.prototype.transform = function(text) {
+	var rpl='';
+	
+	this.quit=false;
+	// unify text string
+	text=text.toLowerCase();
+	text=text.replace(/@#\$%\^&\*\(\)_\+=~`\{\[\}\]\|:\;<>\/\\\t/g, ' ');
+	//	text=text.replace (/\s+-+\s+/g, '.');
+	text=text.replace(/\s*[\.\;]+\s*/g, ' ');// removed "," and "?" original (/\s*[,\.\?!;]+\s*/g, '.')
+	text=text.replace('.');//original had "but but i removed it since it caused a problem when user typed "but". not sure what might be affected by removing. original.    /\s*\bbut\b\s*/g,
+	text=text.replace(/\s{2,}/g, ' ');
+	// split text in part sentences and loop through them
+	var parts=text.split('.');
+	for (var i=0; i<parts.length; i++) {
+		var part=parts[i];
+		if (part!='') {
+			// check for quit expression
+			for (var q=0; q<loverQuits.length; q++) {
+				if (loverQuits[q]==part) {
+					this.quit=true;
+					return this.getFinal();
+				}
+			}
+			// check for quit expression
+			for (var q=0; q<loverQuits2.length; q++) {
+				if (loverQuits2[q]==part) {
+					this.quit=true;
+					return this.getFinal1();
+				}
+			}
+			// check for quit expression
+			for (var q=0; q<loverQuits3.length; q++) {
+				if (loverQuits3[q]==part) {
+					this.quit=true;
+					return this.getFinal3();
+				}
+			}
+		
+			// preprocess (v.1.1: work around lambda function)
+			var m=this.preExp.exec(part);
+			if (m) {
+				var lp='';
+				var rp=part;
+				while (m) {
+					lp+=rp.substring(0,m.index)+this.pres[m[1]];
+					rp=rp.substring(m.index+m[0].length);
+					m=this.preExp.exec(rp);
+				}
+				part=lp+rp;
+			}
+			this.sentence=part;
+			// loop trough keywords
+			for (var k=0; k<loverKeywords.length; k++) {
+				if (part.search(new RegExp('\\b'+loverKeywords[k][0]+'\\b', 'i'))>=0) {
+					rpl = this._execRule(k);
+				}
+				if (rpl!='') return rpl;
+			}
+		}
+	}
+	// // nothing matched try mem
+	// rpl=this._memGet();
+	// // if nothing in mem, so try xnone
+	// if (rpl=='') {
+	// 	this.sentence=' ';
+	// 	var k=this._getRuleIndexByKey('xnone');
+	// 	if (k>=0) rpl=this._execRule(k);
+	// }
+	
+	// // return reply or default string
+	// return (rpl!='')? rpl : 'I am at a loss for words.';
+		// nothing matched try mem
+		rpl=this._memGet();
+		// if nothing in mem, so try xnone
+		if (rpl=='') {
+			this.sentence=' ';
+			var k=this._getRuleIndexByKey('xnone');
+			if (k>=0) rpl=this._execRule(k);
+		}
+		
+		// return reply or default string
+		return (rpl!='')? rpl : 'please pick a number [1] [2] [3]';
+}
+
+loverbot.prototype._execRule = function(k) {
+	var rule=loverKeywords[k];
+	var decomps=rule[2];
+	var paramre=/\(([0-9]+)\)/;
+	for (var i=0; i<decomps.length; i++) {
+		var m=this.sentence.match(decomps[i][0]);
+		if (m!=null) {
+			var reasmbs=decomps[i][1];
+			var memflag=decomps[i][2];
+			var ri= (this.noRandom)? 0 : Math.floor(Math.random()*reasmbs.length);
+			if (((this.noRandom) && (this.lastchoice[k][i]>ri)) || (this.lastchoice[k][i]==ri)) {
+				ri= ++this.lastchoice[k][i];
+				if (ri>=reasmbs.length) {
+					ri=0;
+					this.lastchoice[k][i]=-1;
+				}
+			}
+			else {
+				this.lastchoice[k][i]=ri;
+			}
+			var rpl=reasmbs[ri];
+			if (this.debug) alert('match:\nkey: '+loverKeywords[k][0]+
+				'\nrank: '+loverKeywords[k][1]+
+				'\ndecomp: '+decomps[i][0]+
+				'\nreasmb: '+rpl+
+				'\nmemflag: '+memflag);
+			if (rpl.search('^goto ', 'i')==0) {
+				ki=this._getRuleIndexByKey(rpl.substring(5));
+				if (ki>=0) return this._execRule(ki);
+			}
+			// substitute positional params (v.1.1: work around lambda function)
+			var m1=paramre.exec(rpl);
+			if (m1) {
+				var lp='';
+				var rp=rpl;
+				while (m1) {
+					var param = m[parseInt(m1[1])];
+					// postprocess param
+					var m2=this.postExp.exec(param);
+					if (m2) {
+						var lp2='';
+						var rp2=param;
+						while (m2) {
+							lp2+=rp2.substring(0,m2.index)+this.posts[m2[1]];
+							rp2=rp2.substring(m2.index+m2[0].length);
+							m2=this.postExp.exec(rp2);
+						}
+						param=lp2+rp2;
+					}
+					lp+=rp.substring(0,m1.index)+param;
+					rp=rp.substring(m1.index+m1[0].length);
+					m1=paramre.exec(rp);
+				}
+				rpl=lp+rp;
+			}
+			rpl=this._postTransform(rpl);
+			if (memflag) this._memSave(rpl)
+			else return rpl;
+		}
+	}
+	return '';
+}
+
+loverbot.prototype._postTransform = function(s) {
+	// final cleanings
+	s=s.replace(/\s{2,}/g, ' ');
+	s=s.replace(/\s+\./g, '.');
+	if ((this.global.loverPostTransforms) && (loverPostTransforms.length)) {
+		for (var i=0; i<loverPostTransforms.length; i+=2) {
+			s=s.replace(loverPostTransforms[i], loverPostTransforms[i+1]);
+			loverPostTransforms[i].lastIndex=0;
+		}
+	}
+	// capitalize first char (v.1.1: work around lambda function)
+	if (this.capitalizeFirstLetter) {
+		var re=/^([a-z])/;
+		var m=re.exec(s);
+		if (m) s=m[0].toUpperCase()+s.substring(1);
+	}
+	return s;
+}
+
+loverbot.prototype._getRuleIndexByKey = function(key) {
+	for (var k=0; k<loverKeywords.length; k++) {
+		if (loverKeywords[k][0]==key) return k;
+	}
+	return -1;
+}
+
+loverbot.prototype._memSave = function(t) {
+	this.mem.push(t);
+	if (this.mem.length>this.memSize) this.mem.shift();
+}
+
+loverbot.prototype._memGet = function() {
+	if (this.mem.length) {
+		if (this.noRandom) return this.mem.shift();
+		else {
+			var n=Math.floor(Math.random()*this.mem.length);
+			var rpl=this.mem[n];
+			for (var i=n+1; i<this.mem.length; i++) this.mem[i-1]=this.mem[i];
+			this.mem.length--;
+			return rpl;
+		}
+	}
+	else return '';
+}
+
+loverbot.prototype.getFinal = function() {
+	if (!loverbot.prototype.global.loverFinals) return '';
+	return loverFinals[Math.floor(Math.random()*loverFinals.length)];
+}
+
+loverbot.prototype.getFinal1 = function() {
+	if (!loverbot.prototype.global.loverFinals2) return '';
+	return loverFinals2[Math.floor(Math.random()*loverFinals2.length)];
+}
+
+loverbot.prototype.getFinal3 = function() {
+	if (!loverbot.prototype.global.loverFinals3) return '';
+	return loverFinals3[Math.floor(Math.random()*loverFinals3.length)];
+}
+
+loverbot.prototype.getInitial = function() {
+	if (!loverbot.prototype.global.loverInitials) return '';
+	return loverInitials[Math.floor(Math.random()*loverInitials.length)];
+}
+
+
+// fix array.prototype methods (push, shift) if not implemented (MSIE fix)
+if (typeof Array.prototype.push == 'undefined') {
+	Array.prototype.push=function(v) { return this[this.length]=v; };
+}
+if (typeof Array.prototype.shift == 'undefined') {
+	Array.prototype.shift=function() {
+		if (this.length==0) return null;
+		var e0=this[0];
+		for (var i=1; i<this.length; i++) this[i-1]=this[i];
+		this.length--;
+		return e0;
+	};
+
+	function think(){ ///------------TESTING THINKING TIME
+		document.loverbot.input.value = "";        
+		if( rpl != ' ' ){ respond(); }		
+		else { setTimeout("think()", 2150); }
+	}
+}
+
+// eof
---
+++ V-V/loverdata2.js
@@ -0,0 +1,161 @@
+
+var loverInitials = [
+
+
+
+];
+
+var loverFinals = [
+"bye! (^-^)"
+
+];
+
+var loverFinals2 = [
+
+
+
+];
+
+var loverFinals3 = [
+
+];
+
+
+var loverQuits = [
+"bye",
+"bye bro",
+"goodbye",
+"good bye",
+"done",
+"exit",
+"see you later",
+"gotta go",
+"gotta leave",
+"shutup",
+"self distruct",  
+"leave me alone",
+"goodbye lover",
+"good bye lover",
+"bye lover",
+"i got to go",
+"i've got to go",
+"peace"
+
+];
+
+var loverQuits2 = [ 
+"fuck you",
+"fuck u",
+"i hate you",
+"bitch",
+"fuckoff",
+"pussy",
+"cunt",
+"stupid computer",
+"stupid bot",
+"suck my dick",
+"blow me",
+"you gay",
+"your gay",
+"youre gay",
+"you're gay",
+"ur gay",
+"gay",
+"your stupid",
+"youre stupid",
+"you're stupid",
+"your trash",
+"you're trash",
+"youre trash",
+"you are trash", 
+"kill your self",
+"kill yourself",
+"kill ur self",
+"hoe"
+];
+
+var loverQuits3 = [
+"reboot",
+"restart",
+"rebuild database",
+"shutdown",
+"quit"
+];
+
+
+var loverPres = [
+  'happy','1',
+  'love','1',
+  'two','2',
+  'sad','2',
+  'horny','3',
+  'nsfw','3',
+  'sex','3',
+  'adult','3'
+
+];
+
+var loverPosts = [
+
+];
+
+var loverSynons = {
+
+
+
+
+};
+
+
+
+
+var loverKeywords = [
+
+
+
+
+["1", 60, [
+["*", [
+  "%n%n%n%n[1] 💖 %n%n_\nIt’s okay to be scared.%n%nIt’s okay to be vulnerable and allow yourself to love.%n%n(ㆆ_ㆆ)%n%nbelieve me,%nyou’ll look back on life regretting and that’s%nworse than any fear on earth.%n%n ",
+  "%n%n%n%n[1] 💖 %n%n%n%n\nTell them how you feel✨,%n%nregardless the outcome....%n%nstop being such a pussy😐, they might feel the same....🦋",
+  "%n%n%n%n[1] 💖 %n%n%n%n%nIt’s like 6am and I can’t get your cute smile out my head,%nI can’t believe that we’re getting married today.%n_%n%nTo think I was one wrong bus stop away%nfrom never meeting you..... 💖"
+
+]]
+]],
+
+
+
+
+["2", 60, [
+["*", [
+"%n%n%n%n[2] 🥺 %n%n\nYou were the best thing to happen to me, and I fucked up and lost you.\nI’m so so so sorry that I hurt you Megs, truly.\nI hope you’re doing well, or at least better than I am.\nAs long as you’re happy, that’s what matters to me.\nAnyways, if you’ve read this far you’re probably getting bored of it, so I’ll wrap up.\n%nThank you for even reading this..\n%nHappy Valentines Megan.",
+
+'%n%n%n%n%n[2] 🥺 %n\n%nThe moral of the story is i end up never feeling enough.%n\nSo I never tell them how I truely feel about them.\nAs a result I miss out on potential relationships , and while they\nmove on with the next dude, I’m left with all these feelings with no\nwhere to go and it makes it even harder to share my feelings to\nsomeone I’m interested in.', 
+
+"[2] 🥺 %n\nHow does it feel?\nHow does it feel to lose the one who actually cared about you?\n%nDoes she motivate you and uplift you when you’re feeling down and you want to give up on everything?\nDoes she talk about the future and what it can hold?\nDoes she love you and cater to you the way I did?%n%nWell, if she does, good for you. I hope you're happy now.\nOh, and don’t fuck it up and cheat.\nIf your not happy, just leave.\nIt’ll save her from a traumatic situation.",
+"[2] 🥺%n%n%n%n▾%nSpent $3000 to get a homeless dude with an STD to sleep with my%ncheating ex.%n%nSpent most of the money on making him look \"fuckable\", made%nhim a fake tinder, and rented a motel.%n%nIt only took her a week to fall for the bait.%n%nHoe belonged to the \"streets\" anyway.",
+"[2] 🥺%n%nMet his family too, did whatever a couple would do and shit,%nevery relationship was the same, he would be madly in love%n%nwhile I couldn't wait for it to be over.....%n%nIn hindsight all the guys I dated were stepping stones,%nand with every nut brought me closer to opening doors for my career.%n%nI made a promise to myself, that I would become successful%nand make it to the top,%n%neven if i've got to fuck my way up there."
+
+
+
+
+
+]]
+]],
+
+
+
+["3", 60, [
+["*", [
+
+"%n%n[3] 💦  \n%n\nI remember the first couple times we fucked\n_\nIt was everything I could imagine it to be... so tight, warm, and wet.I barely lasted 5 minutes, we fucked everywhere, in the school gym\nat her aunts house, she even sucked me off\nfor a few seconds in the bible room of our church before we heard\nfootsteps.\n_\nWe could never keep our hands off each other.\nuntil she missed her period...",
+"%n%n%n%n%n[3] 💦 %n%n_%n%nI’m straight and I like it in the ass, don’t judge unless you’ve%ntried it.%n%nif you’re wondering how it feels, well...you know%nhow good scratching an itch feels?,%n%nnow imagine that feeling from \"inside\"",
+"[3] 💦%n%n%nthe day I lost my virginity,%nIt was with my childhood best friend, we got drunk and we both%nknew what lesbian sex was but never got remotely near it.%n%nBut we both knew if we were to do it would be with someone%nwe trusted.%n%nI'll cut out the details of how it went, but it didn't look%na thing like porn, more like two sticks rubbing against each other%nand lot of pain. We surprisingly stayed friends and joke about it fromtime to time."
+
+]]
+]],
+
+
+
+
+];
